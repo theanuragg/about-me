b7cf2c71981e3e9aae88d57045127bca227f98ee
anurag <anurags9693@gmail.com>
2025-11-27 15:07:21 +0530

    Refactor code structure for improved readability and maintainability

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'pbs.twimg.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'slelguoygbfzlpylpxfs.supabase.co', pathname: '/**' },
+      { protocol: 'https', hostname: 'relayer.theaanurag.xyz', pathname: '/**' },
+      { protocol: 'https', hostname:'uptime.theaanurag.xyz', pathname: '/**' },
+      { protocol: 'https', hostname:'avatars.githubusercontent.com', pathname: '/**' },
+
+      // Add example.com to remotePatterns
+    ],
+  },
+  /* other config options */
 };
 
 export default nextConfig;
